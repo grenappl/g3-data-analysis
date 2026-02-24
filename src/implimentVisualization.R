af3def01aa7d3a62b716fb69cc14a1ca48aac104
forgottenpotat <florentinoianmiguel@gmail.com>
2026-02-24 22:29:51 +0800

    turned it to func and yea

--- src/implimentVisualization.R
+++ src/implimentVisualization.R
@@ -1,20 +1,23 @@
-library("plotly")
-set.seed(123)
-eda_data <- data.frame(
-  StudentID = 1:120,
-  StudyHours = runif(120, 1, 12),
-  Attendance = runif(120, 60, 100),
-  ScienceScore = round(
-    40 + 4 * runif(120, 1, 12) + rnorm(120, 0, 8)
-  ),
-  Month = rep(month.abb[1:6], each = 20)
-)
-plot_ly(
-  eda_data,
-  x = ~StudyHours,
-  y = ~ScienceScore,
-  type = "scatter",
-  mode = "markers"
-)
+make_boxplot <- function() {
+  library(plotly)
+  set.seed(123)
+  
+  eda_data <- data.frame(
+    StudentID = 1:120,
+    StudyHours = runif(120, 1, 12),
+    Attendance = runif(120, 60, 100),
+    ScienceScore = round(
+      40 + 4 * runif(120, 1, 12) + rnorm(120, 0, 8)
+    ),
+    Month = rep(month.abb[1:6], each = 20)
+  )
+  
+  p <- ggplot(eda_data,
+              aes(x = Month, y = ScienceScore, fill = Month)) +
+    geom_boxplot() +
+    labs(title = "Box and Whisker Plot")
+  
+  ggplotly(p)
+}
 
-
+make_boxplot()
